84ae95a9e9d7e248572e15377e3ffe52fc8693b8
Viktor <viktor.ganshin@gmail.com>
2024-05-30 00:20:29 +0300

    Homework_3.1

---
+++ Homework_3.1(Simple_Calc).py
@@ -0,0 +1,22 @@
+num1 = float((input('Введіть перше число:')))
+num2 = float((input('Введіть друге число:')))
+oper = input('Введіть запропонований математичний оператор(+, - , *, /)')
+if oper == '/' and num2 == 0:
+    print('На нуль ділити не можна')
+else:
+    if oper == '+':
+        res = num1 + num2
+    elif oper == '-':
+        res = num1 - num2
+    elif oper == '*':
+        res = num1 * num2
+    elif oper == '/' and num2 != 0:
+        res = num1 / num2
+    else:
+        res = None
+        print('Помилка')
+    if res is not None:
+        if res % 1 == 0:
+            print(int(res))
+        else:
+            print(res)
