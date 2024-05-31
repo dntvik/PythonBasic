dfeb60d6b143f957aef5d1b7436f0676d66cd3a6
Viktor <viktor.ganshin@gmail.com>
2024-05-31 22:37:32 +0300

    add test ignore HomeWork_3.3

---
+++ Homework_3.3.py
@@ -0,0 +1,12 @@
+lst = [1, 2, 3]
+lenl = len(lst)
+mid = (len(lst)+1)//2
+lst1 = lst[:mid]
+lst2 = lst[mid:]
+lst3 = [lst1, lst2]
+if lenl == 0:
+    lst.append([])
+    lst *= 2
+    print(lst)
+else:
+    print(lst3)
