260a9859365b31e57dacd0b0746edc939758fd1b
Viktor <viktor.ganshin@gmail.com>
2024-06-12 02:24:53 +0300

    add dict in hw_6.2

--- Homework_6.2(second_calendar).py
+++ Homework_6.2(second_calendar).py
@@ -5,13 +5,13 @@ else:
     cont_minutes, sec = divmod(input_sec, 60)
     cont_hours, minutes = divmod(cont_minutes, 60)
     days, hours = divmod(cont_hours, 24)
-    days_name = ('День', 'Дні', 'Днів')
+    days_name = {1: 'День', 2: 'Дні', 3: 'Днів'}
     days_name_true = 0
     if days % 10 == 1:
-        days_name_true = days_name[0]
-    if 2 <= days % 10 <= 4:
         days_name_true = days_name[1]
-    if days % 10 >= 5 or days % 10 == 0 or 11 <= days <= 19:
+    if 2 <= days % 10 <= 4:
         days_name_true = days_name[2]
+    if days % 10 >= 5 or days % 10 == 0 or 11 <= days <= 19:
+        days_name_true = days_name[3]
     time = str(hours).zfill(2) + ':' + str(minutes).zfill(2) + ':' + str(sec).zfill(2)
-    print(str(input_sec) + ' -> ' + str(days), days_name_true, time)
+    print(str(input_sec) + ' -> ' + str(days), days_name_true + ',', time)
